7f01e7336f2e225a3cad124eac706dd52ec901a3
Kleis Hamzai <kleishamzai123@gmail.com>
2024-09-14 21:02:49 +0000

    try

--- public/Collectible.mjs
+++ public/Collectible.mjs
@@ -1,16 +1,33 @@
-class Collectible {
-  constructor({x, y, value, id}) {
+const playFieldOffsetLeft = 8;
+const playFieldOffsetTop = 50;
 
+const collectibleSprite =  {
+    width: 38,
+    height: 18,
+    srcs: '/images/Rahmen.png'
+  }
+const spriteWidth = collectibleSprite.width;
+const spriteHeight = collectibleSprite.height;
+
+class Collectible {
+  constructor({ x, y, value = 1 }) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+ 
   }
 
-}
+  setState({ x, y, value = 1 }) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+  }
 
-/*
-  Note: Attempt to export this for use
-  in server.js
-*/
-try {
-  module.exports = Collectible;
-} catch(e) {}
+  draw(ctx, sprites) {
+    const x = this.x + playFieldOffsetLeft;
+    const y = this.y + playFieldOffsetTop;
+    ctx.drawImage(sprites, x, y, spriteWidth, spriteHeight);
+  }
+}
 
 export default Collectible;
--- server.js
+++ server.js
@@ -1,56 +1,166 @@
 require('dotenv').config();
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const expect = require('chai');
-const socket = require('socket.io');
+const socketio = require('socket.io');
+const helmet = require('helmet');
+const nocache = require('nocache');
 const cors = require('cors');
+import Collectible from './public/Collectible.mjs';
+
+const {
+    playerJoin,
+    getPlayers,
+    playerLeave,
+    setPlayerState,
+} = require('./public/playerFunctions');
 
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner.js');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(
+    helmet({
+        noSniff: true,
+        xssFilter: true,
+        hidePoweredBy: {
+            setTo: 'PHP 7.4.3',
+        },
+    })
+);
+app.use(nocache());
 
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/assets', express.static(process.cwd() + '/assets'));
+app.use('/images', express.static(process.cwd() + '/images'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-//For FCC testing purposes and enables user to connect from outside the hosting platform
-app.use(cors({origin: '*'})); 
 
-// Index page (static HTML)
+app.use(cors({
+    origin: '*'
+}));
+
 app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  }); 
+    .get(function(req, res) {
+        res.sendFile(process.cwd() + '/views/index.html');
+    });
 
-//For FCC testing purposes
 fccTestingRoutes(app);
-    
+
 // 404 Not Found Middleware
 app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+    res.status(404)
+        .type('text')
+        .send('Not Found');
 });
+
+// Randomly generate collectible item's position
+const playField = {
+    width: 624,
+    height: 422
+}
+const collectibleSprite = {
+    width: 38,
+    height: 18,
+    srcs: '/images/Rahmen.png'
+}
+
+function generateStartPos(playField, sprite) {
+    return {
+        x: Math.random() * (playField.width - sprite.width),
+        y: Math.random() * (playField.height - sprite.height),
+    };
+}
+
+
+const collectiblePos = generateStartPos(playField, collectibleSprite);
+
+const collectible = new Collectible({
+    x: collectiblePos.x,
+    y: collectiblePos.y
+});
+
+io.on('connection', (socket) => {
+    socket.on('newPlayerJoined', (player) => {
+        const currentPlayers = getPlayers();
+        socket.emit('currentOpponents', currentPlayers);
+
+        socket.emit('collectible', collectible);
+
+        const currentPlayer = playerJoin(player);
+        socket.broadcast.emit('newOpponent', currentPlayer);
+
+    });
+
+    socket.on('playerStateChange', (player) => {
+        const updatedPlayer = setPlayerState(player);
+        socket.broadcast.emit('opponentStateChange', updatedPlayer);
+    });
+
+    socket.on('playerCollideWithCollectible', (player) => {
+        player.score += 1
+    
+        socket.emit('scored', player.score);
+
+        const updatedPlayer = setPlayerState(player);
+        socket.broadcast.emit('opponentStateChange', updatedPlayer);
+
+
+        // Generate new position for the collectible item
+        let newCollectiblePos = generateStartPos(playField, collectibleSprite);
+        
+        
+        while (
+            newCollectiblePos.x === collectible.x &&
+            newCollectiblePos.y === collectible.y
+        ) {
+            newCollectiblePos = generateStartPos(playField, collectibleSprite);
+        }
+
+
+        collectible.setState({
+            x: newCollectiblePos.x,
+            y: newCollectiblePos.y,
+        });
+        // Send the new(updated) collectible item to all clients
+        io.sockets.emit('collectible', collectible);
+    });
+
+    // Run when player disconnects
+  socket.on('disconnect', () => {
+    const player = playerLeave(socket.id);
+    socket.broadcast.emit('opponentLeave', player.id);
+  });
+
+})
+
+
+
+
 
 const portNum = process.env.PORT || 3000;
 
 // Set up server and tests
-const server = app.listen(portNum, () => {
-  console.log(`Listening on port ${portNum}`);
-  if (process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch (error) {
-        console.log('Tests are not valid:');
-        console.error(error);
-      }
-    }, 1500);
-  }
+server.listen(portNum, () => {
+    console.log(`Listening on port ${portNum}`);
+    if (process.env.NODE_ENV === 'test') {
+        console.log('Running Tests...');
+        setTimeout(function() {
+            try {
+                runner.run();
+            } catch (error) {
+                console.log('Tests are not valid:');
+                console.error(error);
+            }
+        }, 1500);
+    }
 });
 
-module.exports = app; // For testing
+module.exports = server; // For testing
